94a6e8bb0111812c11056a8a5434adbb55cdabc6
IonutStef <Dinu.Ionut.Stef@gmail.com>
2016-07-09 19:55:12 +0300

    HelloWorld adaugat

---
+++ ConsoleServerHost/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleServerHost
+{
+    class HelloWorld
+    {
+        public void sayHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
